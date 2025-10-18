2ac6f9d5732771fef31198e3a7fdab39db5fc256
schaman1 <153807071+schaman1@users.noreply.github.com>
2025-10-18 18:11:08 +0200

    Add files via upload

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+# CHANGELOG
+
+### [0.0.1] - 2025-10-06
+- card system + display
+
+### [0.0.1] - 2025-10-05
+- menu
+
+### Initial.2 - 2025-10-05
+- Structure + menu
+
+### Initial - 2025-10-02
+- Création des fichiers de base au gamedev
---
+++ setup.py
@@ -0,0 +1,11 @@
+#Sera utile plus tard pour publier ce jeu sur Pypi (https://pypi.org/) par ex :
+
+
+#En termes plus simples, setup.py est un script contenu dans les packages ou les bibliothèques
+#pour assurer leur installation complète et correcte.
+#Le script setup.py est écrit et utilise le langage de programmation Python.
+
+#Si le package que nous avons extrait dans un répertoire contient un fichier setup.py, 
+#nous pouvons installer directement ce package en exécutant la commande suivante.
+
+#python setup.py install
