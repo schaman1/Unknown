de6e43c13afb18b49c43dd433520583aaad5fc30
Timothee <nobilettimothee@gmail.com>
2025-11-30 17:42:58 +0100

    Mergo done properly

--- game/client/C_game.py
+++ game/client/C_game.py
@@ -1,5 +1,6 @@
 import pygame
 from client.Personnages_client.perso1 import Player
+from client.Monster_client.C_monster_all import Monster_all
 import var
 
 class Game :
@@ -13,7 +14,7 @@ class Game :
         self.bg = pygame.image.load("assets/bgGlobal.png").convert()
         self.bg = pygame.transform.scale(self.bg, (canva_size[0],canva_size[1]))
         self.light = pygame.Surface((canva_size[0],canva_size[1]), pygame.SRCALPHA)
-        #self.create_light(vision = var.NBR_CELL_CAN_SEE)
+        self.create_light(vision = var.NBR_CELL_CAN_SEE)
         self.player = Player("assets/playerImg.png",self.cell_size, 500, 500)
 
 
@@ -63,9 +64,16 @@ class Game :
         #    None
         #pygame.draw.rect(self.canva, color, self.rect_grid[y][x])
 
+    def blit_monster(self,screen,x,y):
+        self.monsters.blit_all_monster()
+        screen.blit(self.monsters.canva_monster(x,y))
+
     def draw(self,screen,x,y):
         screen.blit(self.bg,(0,0))
         screen.blit(self.canva, (x, y))
+
+        self.blit_monster(screen,x,y)
+
+
         screen.blit(self.light,(0,0))
         screen.blit(self.player.Img_perso, self.center)
-        #screen.blit(a, self.center)
