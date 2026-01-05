10654e0e450c93939a5364082787a0490dd3a150
Timothee <nobilettimothee@gmail.com>
2026-01-05 19:27:31 +0100

    clean architecture

--- game/serv/core/server.py
+++ game/serv/core/server.py
@@ -1,4 +1,5 @@
-import socket, threading, struct, select, sys
+import socket, threading, sys
+from serv.core.network_handler import Network_handler
 from serv.systems.map.read_map import Read_map
 from serv.systems.monster.read_monster import Read_monster
 from shared.constants import world
@@ -11,8 +12,8 @@ class Server:
     """Class mere mais ! 1 pour tout le jeu = on partage tous la même"""
     def __init__(self,port=network.PORT,host='0.0.0.0'):
         self.lClient = {}
-        self.buffers = {}
-
+        
+        self.network_handler = Network_handler(self)
         self.map_cell = Read_map(assets.BG_CELL)
         self.map_monster = Read_monster(assets.BG_MONSTER,world.SIZE_CHUNK_MONSTER,world.RATIO,self.map_cell.dur,self.map_cell.vide,self.map_cell.liquid)
 
@@ -25,82 +26,6 @@ class Server:
         self.current_thread = None
         self.nbr_player = 0
         #self.intervalle_available_server = intervalle
-
-    def handle_clients(self):
-        """Gère la réception des messages d'un client connecté. = Chaque client à sa boucle handle_client"""
-        
-        #try:
-        #    buffer = bytearray()
-        #    while True:
-
-        sockets = list(self.lClient.keys())
-              
-         # Ne pas appeler select sur une liste vide
-        if len(sockets) == 0:
-            return
-        
-        # select non bloquant (timeout = 0)
-        readable, _, _ = select.select(sockets, [], [], 0)
-
-        for client_socket in readable :
-            try:
-                data = client_socket.recv(1024)
-
-            except BlockingIOError as e:
-                    continue
-            
-            except Exception as e : 
-                    print("Erreur reception :",e)
-
-            if not data:
-                print(f"Client déconnecté proprement.")
-
-                self.remove_client(client_socket)
-                continue
-
-            # ajouter les données au buffer du client
-            buffer = self.buffers[client_socket]
-            buffer.extend(data)
-
-            while True:
-                if len(buffer) < 1:
-                    break
-
-                msg_id = buffer[0]
-
-                # Exemple : ID 0 = start_game (1 byte)
-                if msg_id == 0: #Start game
-                    msg_size = 1
-
-                elif msg_id == 1: #
-                    msg_size = 1 + 4 
-
-                elif msg_id==3:
-                    msg_size=1+1
-                
-                elif msg_id==4:
-                    msg_size=1
-
-                else:
-                    print("UNKNOWN MSG ID", msg_id)
-                    del buffer[0]
-                    continue
-
-                if len(buffer) < msg_size:
-                    break  # message incomplet
-
-                msg = buffer[:msg_size]
-                del buffer[:msg_size]
-
-                # Traitement
-                self.process_message(client_socket,msg,msg_size)
-
-    def process_message(self,client_socket,msg,msg_size):
-
-        if self.is_running_menu:
-            self.in_menu(msg, client_socket)
-        elif self.is_running_game:
-            self.in_game(msg, client_socket)
 
     def safe_peername(self, sock):
         """Renvoie une adresse lisible ou 'inconnue' si la socket est fermée."""
@@ -125,176 +50,12 @@ class Server:
             except:
                 pass
 
-            if client_socket in self.buffers:
-                del self.buffers[client_socket]
+            if client_socket in self.network_handler.buffers:
+                del self.network_handler.buffers[client_socket]
 
             del self.lClient[client_socket]
         else:
             print(f"Tentative de suppression d’un client déjà supprimé.")
-
-    def in_menu(self, data, sender):
-        """Traite les données sachant qu'on est dans le menu"""
-        id_msg = struct.unpack("!B", data[0:1])[0]
-
-        if id_msg == 1: #New client connection
-
-            print("New client connection")
-
-            self.send_client_already_her(sender)
-            screen_size = struct.unpack("!HH", data[1:5])
-
-            self.set_screen_size_client(sender,screen_size)
-            
-            for client in list(self.lClient.keys()):
-
-                meornot = (client == sender)
-                packet = struct.pack("!BBB", 1, self.nbr_player, meornot)
-                self.send_data(packet,client)
-
-                #self.send_data({
-                #    "id": "new player",
-                #    "new connection": text,
-                #    "sender": meornot
-                #}, client)
-
-        elif id_msg == 2:
-            print("Remove client")
-            removed_id = self.lClient[sender].id
-            self.remove_client(sender)
-
-            for client in list(self.lClient.keys()):
-
-                packet = bytearray()
-                packet+=struct.pack("!BB", 2,removed_id)
-                self.send_data(packet,client)
-
-                #self.send_data({
-                #    "id": "remove player",
-                #    "remove connection": removed_id
-                #}, client)
-
-        elif id_msg == 0: #1 = start game
-            #Redirige vers le game, stop le loop_server_in_menu pour start celui de in_game
-            print("start !")
-            self.is_running_menu = False
-            self.is_running_game = True
-
-    def in_game(self,data,sender):
-        """Traite les données sachant qu'on est en jeu = saute par ex"""
-
-        id_msg = struct.unpack("!B", data[0:1])[0]
-
-        if id_msg == 3 :
-
-            dep = struct.unpack("!B", data[1:2])[0]
-            self.lClient[sender].move_from_key(dep,self.map_cell.grid_type,self.map_cell.dur,self.map_cell.vide,self.map_cell.liquid)
-
-        elif id_msg == 4 :
-            pass
-
-
-        else :
-            print("What to do with this id send ? ",id_msg)
-
-    def send_data_all(self,data):
-        """Permet d'envoyer data a tout les clients connecté au jeu data = dico"""
-        def send_to(socket):
-            try:
-                #print("Send successfuly")
-                self.send_data(data, socket)
-            except Exception as e:
-                #print(data)
-                print(f"Erreur envoi, {e}",file=sys.stderr)
-                pass  # ou suppression du client mort
-
-        for socket in self.lClient.keys():
-            threading.Thread(target=send_to, args=(socket,), daemon=True).start()
-
-    def send_data_update(self,data,id):
-        """Permet d'envoyer data a tout les clients connecté au jeu data = dico"""
-        #print(data)
-        def send_to(socket,message):
-            try:
-                #print("Send successfuly")
-                self.send_data(message, socket)
-            except Exception as e:
-                print(f"Erreur envoi bis {e}")#,file=sys.stderr)
-                pass  # ou suppression du client mort
-
-        for cnt,socket in enumerate(self.lClient.keys()):
-            if len(data[cnt]) != 0:
-
-                threading.Thread(target=send_to, args=(socket,[id,data[cnt]]), daemon=True).start()
-
-    def pack_cells(self,cells,packet):
-
-        # nombre de cellules
-        packet += struct.pack("!H", len(cells))
-
-        # données des cellules
-        for (x, y, r, g, b, a) in cells:
-            packet += struct.pack("!hhBBBB", x, y, r, g, b, a)
-
-        return bytes(packet)
-    
-    def pack_monsters(self,monsters,packet):
-
-        #print("Pack monsters")
-
-        # nombre de cellules
-
-        packet += struct.pack("!H", len(monsters))
-
-        # données des cellules
-        for (chunk, id, x, y) in monsters:
-            packet += struct.pack("!HLHH", chunk, id, x, y)
-
-        return bytes(packet)
-
-    def send_data(self, data, client):
-        """Envoie des données à un client spécifique."""
-
-        packet = bytearray()
-        id = data[0]
-        packet += struct.pack("!B", id)   # envoie l’ID du message (1 octet)
-
-        if id == 0 :
-            pass
-
-        elif id == 1 : 
-            packet+= struct.pack("!BB",data[1],data[2])
-
-        elif id == 2: #rem client
-            pass
-
-        elif id == 3:
-            self.pack_cells(data[1],packet)
-
-        elif id == 4:
-            self.pack_monsters(data[1],packet)
-
-        elif id==5:
-            self.pack_monsters(data[1],packet)
-            
-        elif id==6:
-            packet+=struct.pack("!BLL",data[1],data[2],data[3])
-
-        else :
-            print("Issue id not found : ",id)
-
-        try:
-
-            client.send(packet)
-
-        except OSError:
-            # Déconnexion
-            is_host = self.lClient.get(client, {}).get("Host", False)
-            if is_host:
-                print("Le host a quitté, fermeture du serveur.")
-                self.stop_server()
-
-        except Exception as e:
-            print(f"Erreur envoi: {e}")
 
     def stop_server(self):
         """Arrête le serveur et déconnecte tous les clients."""
@@ -394,35 +155,32 @@ class Server:
             #self.set_param_on_client_connection(client_socket)
             #threading.Thread(target=self.handle_client, args=(client_socket,), daemon=True).start()
 
-    def send_client_already_her(self,client):
-        #self.send_data({"id":"set client already connected","clients":self.lClient})
-
-        for player in self.lClient.values():
-
-            packet = struct.pack("!BBB", 1, player.id, 0)
-
-            self.send_data(packet,client)
-                           #{
-                    #"id": "new player",
-                    #"new connection": player.id,
-                    #"sender": False
-                #}, client)
-
     def set_param_on_client_arriving(self,client_socket):
         """Set une fois qu'a reçu la 1er donnée du client"""
         is_host = len(self.lClient) == 0
         self.nbr_player += 1
         self.lClient[client_socket] = Player(pos = world.SPAWN_POINT,id = self.nbr_player,host = is_host)
-        self.buffers[client_socket] = bytearray()
+        self.network_handler.buffers[client_socket] = bytearray()
 
-    def set_screen_size_client(self,client_socket,screen_size):
-        self.lClient[client_socket].set_screen_size(screen_size)
-        
-        #for socket,client in self.lClient.items():
-#
-        #    if socket != client_socket :
-        #        self.send_data({
-        #            "id": "new player",
-        #            "new connection": client.id,
-        #            "sender": False
-        #        }, client_socket)
+    def send_data_update(self,data,id):
+        self.network_handler.send_data_update(data,id)
+
+    def send_data(self,packet,client):
+        self.network_handler.send_data(packet,client)
+
+    def send_data_all(self,data):
+        """Permet d'envoyer data a tout les clients connecté au jeu data = dico"""
+        def send_to(socket):
+            try:
+                #print("Send successfuly")
+                self.send_data(data, socket)
+            except Exception as e:
+                #print(data)
+                print(f"Erreur envoi, {e}",file=sys.stderr)
+                pass  # ou suppression du client mort
+
+        for socket in self.lClient.keys():
+            threading.Thread(target=send_to, args=(socket,), daemon=True).start()
+
+    def handle_clients(self):
+        self.network_handler.handle_clients()
--- game/serv/domain/shoot/shoot_all.py
+++ game/serv/domain/shoot/shoot_all.py
@@ -1,11 +1,16 @@
-
+from serv.domain.shoot import weapon
 
 class Shoot_all :
 
     def __init__(self):
         self.next_id = 0
-        self.dic_Shoot = {}
+        self.l_Shoot = []
 
     def generate_id(self):
         self.next_id = (self.next_id+1) % 65536 #Maximum pour uint16
         return self.next_id
+    
+    def create_shoot(self,type_weapon,angle,pos):
+        
+        if type_weapon == "pioche" :
+            self.l_Shoot.append(weapon.Pioche(self.generate_id(),angle,pos))
