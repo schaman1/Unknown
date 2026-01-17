3f77921752334f95522492900a58eb70ac13efe8
Louan-clst <louan.favre@free.fr>
2026-01-17 02:14:23 +0100

    animation weapon/player direction fixed/updated

--- game/client/domain/mob/player/player.py
+++ game/client/domain/mob/player/player.py
@@ -11,7 +11,8 @@ class Player(Mob) :
 
         self.pseudo = pseudo
         self.is_you = is_you
-        self.frame_perso = []
+        self.frame_perso_left = []
+        self.frame_perso_right = []
         #self.frame_weapon = []
         self.frame = 0
         self.frame_multiplier = 0
@@ -20,12 +21,14 @@ class Player(Mob) :
 
     def init_Img(self,cell_size):
         for i in range(4):
-            Img = pygame.image.load(assets.PLAYER_IDLE+f"{i+1}"+".png").convert_alpha() #convert_alpha() pour le fond vide
-            Img = pygame.transform.scale(Img,(self.width*cell_size,self.height*cell_size))
-            self.frame_perso.append(Img)
-        self.img_weapon = pygame.image.load(assets.PIOCHE).convert_alpha() #pour l'instant c'est juste un projectile
-        self.img_weapon = pygame.transform.scale(self.img_weapon,(self.width*cell_size, self.height*cell_size)) 
-        #self.frame_weapon.append(Img_weapon)
+            img_left = pygame.image.load(assets.PLAYER_IDLE+f"{i+1}"+".png").convert_alpha() #convert_alpha() pour le fond vide
+            img_left = pygame.transform.scale(img_left,(self.width*cell_size,self.height*cell_size))
+            self.frame_perso_left.append(img_left)
+            img_right = pygame.transform.flip(img_left, True, False)
+            self.frame_perso_right.append(img_right) #liste d'image dans l'autre sens pour pas couper l'animation
+
+        self.img_weapon = pygame.image.load(assets.RANGED_WEAPON+"2.png").convert_alpha()
+        self.img_weapon = pygame.transform.scale(self.img_weapon,(self.width*16, self.height*16))
 
 
     def update_frame(self):
@@ -34,29 +37,35 @@ class Player(Mob) :
             self.frame +=1
             self.frame_multiplier = 0
     
-    def get_angle(self, center, mouse_pos) -> int:
+    def get_angle(self, pos, mouse_pos) -> int:
         '''renvoie l'angle entre le perso et la souris en int'''
-        oppose = mouse_pos[1] - center[1]
-        adjacent = mouse_pos[0] - center[0]
+        oppose = mouse_pos[1] - pos[1]
+        adjacent = mouse_pos[0] - pos[0]
         mouse_angle = int(math.degrees(math.atan2(-oppose,adjacent))) #comme atan() mais en 2D
         return mouse_angle
-        
     
-    def draw_weapon(self,screen, mouse_pos,pos):
+    def draw_weapon(self,screen,angle,pos):
         '''modifie l'orientation de l'arme en fonction de l'angle de la souris'''
-        self.angle = self.get_angle(pos, mouse_pos)
-        rotated_img = pygame.transform.rotate(self.img_weapon, self.angle)
+        rotated_img = pygame.transform.rotate(self.img_weapon, angle)
         rotated_polish = rotated_img.get_rect(center = pos)
         screen.blit(rotated_img, rotated_polish.topleft)
-        self.update_frame()
 
     def draw(self,screen,xscreen,yscreen, mouse_pos=None,center=None):
         
         pos = self.calculate_pos_blit(xscreen,yscreen)
-        screen.blit(self.frame_perso[self.frame%4],pos)
+        self.angle_weapon = self.get_angle(pos, mouse_pos)
+        #self.angle = self.get_angle(center, mouse_pos)
+        
+        perso_left = self.frame_perso_left[self.frame%4]
+        perso_right = self.frame_perso_right[self.frame%4] 
+
+        if mouse_pos[0] > center[0]: #affiche le perso regardant une dirrection en fonction de la souris
+            screen.blit(perso_right,pos)
+        else :
+            screen.blit(perso_left, pos)
         self.update_frame()
 
-        self.draw_weapon(screen,mouse_pos,pos)
+        self.draw_weapon(screen,self.angle_weapon,pos)
         
         pygame.draw.rect( #Pour voir où le perso est en temps reel
             screen,
