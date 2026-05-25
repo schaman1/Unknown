2e8be3d68a4ad6c303dd83b6e5f3be26f94cbb47
Timothee <nobilettimothee@gmail.com>
2026-05-25 13:10:20 +0200

    Added FLUFF DIE EFFECT

--- game/client/config/assets.py
+++ game/client/config/assets.py
@@ -58,6 +58,7 @@ SPELLS[42] = resource_path("assets/sprites/projectile/projectile_42_0.png")
 SPELLS[43] = resource_path("assets/sprites/projectile/projectile_43_0.png")
 SPELLS[44] = resource_path("assets/sprites/projectile/projectile_44_0.png")
 SPELLS[45] = resource_path("assets/sprites/projectile/projectile_45_0.png")
+SPELLS[46] = resource_path("assets/sprites/projectile/projectile_46_0.png")
 
 HEALER = resource_path("assets/objects/healer.png")
 HEALER_TRIGGER = resource_path("assets/objects/healer_trigger.png")
