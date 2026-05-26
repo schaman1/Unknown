a064239deca2aba8c9418a10f9cc793f6ec8fbb5
Louan-clst <louan.favre@free.fr>
2026-05-26 20:03:21 +0200

    entities in all map

--- game/serv/config/add_objects_begin.py
+++ game/serv/config/add_objects_begin.py
@@ -1,32 +1,41 @@
 OBJECTS = []
 
-OBJECTS.append(("HEALER",1,8500,15250,0))
-OBJECTS.append(("HEALER",1,35491,28999 - 100,0))
-OBJECTS.append(("HEALER",1,29137, 9000 - 100,0))
-OBJECTS.append(("HEALER",1,84000, 23000 - 100,0))
+OBJECTS.append(("HEALER",1, 8500, 15250, 0))
+OBJECTS.append(("HEALER",1, 35491, 28999 - 100, 0))
+OBJECTS.append(("HEALER",1, 84000, 23000 - 100, 0))
+OBJECTS.append(("HEALER",1, 29137, 11799 - 100, 0))
+OBJECTS.append(("HEALER",1, 68671, 15399 - 100, 0))
+# OBJECTS.append(("HEALER",1, 21270, 32599 - 100, 0))
 
 
-OBJECTS.append(("Chest",1,19238, 16199 +300,10))
-OBJECTS.append(("Chest",1,14075, 19399 +300,10))
-OBJECTS.append(("Chest",1,18500, 11799 +300,20))
-OBJECTS.append(("Chest",1,18419, 21239 +300,20))
 
+#1: commun / 2 = upgrade weapon / 3 : Rare / 4 : legendaire
+OBJECTS.append(("Chest",1, 19238, 16199 +300,10))
+OBJECTS.append(("Chest",1, 27100, 19300,10))
+OBJECTS.append(("Chest",1, 41200, 18199 +300, 10))
+OBJECTS.append(("Chest",1, 14075, 19399 +300, 10))
+OBJECTS.append(("Chest",1, 16383, 9400 +300, 20))
+OBJECTS.append(("Chest",1, 18419, 21400 +300, 20))
+OBJECTS.append(("Chest",1, 29137, 9000 +300, 20))
 
-#OBJECTS.append(("Chest",1,10500,19300,20))
+OBJECTS.append(("Chest", 2, 40950, 8599 +300, 60))
+OBJECTS.append(("Chest", 2, 29700, 23300, 60))
+OBJECTS.append(("Chest", 2, 52400, 21800 +300, 80))
+OBJECTS.append(("Chest", 2, 57400, 7400 +300, 60))
+OBJECTS.append(("Chest", 2, 94600, 28600 +300, 80))
+OBJECTS.append(("Chest", 2, 78600, 45400 +300, 80))
+OBJECTS.append(("Chest", 2, 61200, 30200 +300, 80))
+OBJECTS.append(("Chest", 2, 62150, 35400 +300, 100))
+OBJECTS.append(("Chest", 2, 115650, 45400 +300, 100))
 
-OBJECTS.append(("Chest", 2, 40950, 8599 +300, 20))
-OBJECTS.append(("Chest", 2, 68671, 15399 +300, 20)) ######
-OBJECTS.append(("Chest", 2, 94600, 28600 +300, 20))
-OBJECTS.append(("Chest", 2, 78600, 45400 +300, 20))
+# OBJECTS.append(("Chest",3,6010,221200 +300, 90)) //salle troll
+OBJECTS.append(("Chest",3,82190,30200 +300, 100))
+OBJECTS.append(("Chest",3,118200,31800 +300, 100))
+OBJECTS.append(("Chest",3,105000,45400 +300, 120))
 
-OBJECTS.append(("Chest",3,27100,19300,50))
-OBJECTS.append(("Chest",3,82190,30200 +300,50))
-OBJECTS.append(("Chest",3,118200,31800 +300,50))
-
-OBJECTS.append(("Chest",4,29700,23300,80)) #1: commun / 2 = upgrade weapon / 3 : Rare / 4 : legendaire
-OBJECTS.append(("Chest",4,41200,18199 + 300,80))
-OBJECTS.append(("Chest",4,48298,14599 + 300,80))
-OBJECTS.append(("Chest",4,117500,21800 + 300,80))
+OBJECTS.append(("Chest",4,48298,14599 +300, 170))
+OBJECTS.append(("Chest",4,117500,21800 +300, 170))
+OBJECTS.append(("Chest",4,97331,52600 +300, 170))
 
 OBJECTS.append(("SPELL",2,1300,18500,5)) #Put a pnj instead
 
@@ -38,4 +47,4 @@ OBJECTS.append(("SPELL",2,1300,18500,5)) #Put a pnj instead
 #OBJECTS.append(("UpgradeWeapon",1,5000,16000,0))
 
 WEAPONS_BEGIN = [[],[3],[7],[44]]
-MAGASIN_BEGIN = [(39000,23600 - 70), (66818, 18999 - 100), (27498, 11799 - 100)]
+MAGASIN_BEGIN = [(39000,23600 - 70), (66818, 18999 - 100), (75300, 28000)] #27800
