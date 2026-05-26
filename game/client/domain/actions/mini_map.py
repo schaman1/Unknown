e1574f4c3ccb5861b9031605af0263b66b4434b3
Timothee <nobilettimothee@gmail.com>
2026-05-26 17:46:55 +0200

    Added map !

--- game/client/domain/actions/mini_map.py
+++ game/client/domain/actions/mini_map.py
@@ -2,17 +2,22 @@ import pygame
 
 class MiniMap :
 
-    def __init__(self,vision_size,map_seen,map_unseen,canva_size,cell_size):
+    def __init__(self,vision_size,map_seen,map_unseen,screen_size,cell_size):
 
+        self.size_1_pixel = 240//30
+        #screen_size
+        #map_seen_size
         self.map_seen = pygame.image.load(map_seen).convert()
-        self.map_seen = pygame.transform.scale(self.map_seen, (canva_size[0],canva_size[1]))
+        self.reel_size_map = self.map_seen.get_size()
+        self.scale = screen_size[0]/self.reel_size_map[0]
 
-        self.map_unseen = pygame.Surface((canva_size[0],canva_size[1]), pygame.SRCALPHA)
+        self.map_seen = pygame.transform.scale(self.map_seen, (screen_size[0],self.reel_size_map[1]*self.scale))
+
+        self.map_unseen = pygame.Surface((screen_size[0],screen_size[1]), pygame.SRCALPHA)
         self.map_unseen.fill((0,0,0))
 
-
         self.cell_size = cell_size
-        self.vision = vision_size
+        self.vision = 20#vision_size
         self.draw = False
 
     def draw_circle(self,screen,color,pos,r,width=0):
@@ -21,6 +26,10 @@ class MiniMap :
 
     def draw_map(self,screen,pos):
 
+        pos = [pos[0]//(4*self.size_1_pixel),pos[1]//(4*self.size_1_pixel)]
+
+        pos[0] *=self.scale
+        pos[1] *=self.scale
         self.draw_circle(self.map_unseen,(0,0,0,0),pos,self.vision)
 
         if self.draw :
