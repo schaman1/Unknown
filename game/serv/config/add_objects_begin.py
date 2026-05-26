0e81bdaca6c2ac50907bb66cfd0f0fb03e698218
Louan-clst <louan.favre@free.fr>
2026-05-26 16:03:26 +0200

    début de poser entités

--- game/serv/config/add_objects_begin.py
+++ game/serv/config/add_objects_begin.py
@@ -5,12 +5,16 @@ OBJECTS.append(("HEALER",1,8500,15250,0))
 OBJECTS.append(("Chest",1,18500,12300,20))
 OBJECTS.append(("Chest",1,19100,16000,10))
 OBJECTS.append(("Chest",1,13400,18600,10))
+
 #OBJECTS.append(("Chest",1,10500,19300,20))
+
+OBJECTS.append(("Chest", 2, 40950, 8599, 20))
+OBJECTS.append(("Chest", 2, 68671, 15399 +300, 20)) ######
 
 OBJECTS.append(("Chest",3,27100,19300,50))
 
 OBJECTS.append(("Chest",4,29700,23300,80)) #1: commun / 2 = upgrade weapon / 3 : Rare / 4 : legendaire
-OBJECTS.append(("Chest",4,41200,18300,80))
+OBJECTS.append(("Chest",4,41200,18199 + 300,80))
 
 OBJECTS.append(("SPELL",2,1300,18500,5)) #Put a pnj instead
 
@@ -22,4 +26,4 @@ OBJECTS.append(("SPELL",2,1300,18500,5)) #Put a pnj instead
 #OBJECTS.append(("UpgradeWeapon",1,5000,16000,0))
 
 WEAPONS_BEGIN = [[],[3],[7],[44]]
-MAGASIN_BEGIN = [(28500,12500),(39000,23600),(19500,19200)]
+MAGASIN_BEGIN = [(28500,12500),(39000,23600),(19500,19200), (66818, 18999 - 100)]
