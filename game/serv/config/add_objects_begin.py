b91884ac01d77a66ab7c35670617d42897bcc104
Timothee <nobilettimothee@gmail.com>
2026-05-27 13:48:36 +0200

    All done

--- game/serv/config/add_objects_begin.py
+++ game/serv/config/add_objects_begin.py
@@ -8,8 +8,6 @@ OBJECTS.append(("HEALER",1, 70200, 15800 - 100, 0))
 OBJECTS.append(("HEALER",1, 107700, 49000 - 100, 0))
 # OBJECTS.append(("HEALER",1, 21270, 32599 - 100, 0))
 
-
-
 #1: commun / 2 = upgrade weapon / 3 : Rare / 4 : legendaire
 OBJECTS.append(("Chest",1, 19238, 16199 +225,10))
 OBJECTS.append(("Chest",1, 27100, 19000+225,10))
@@ -17,11 +15,10 @@ OBJECTS.append(("Chest",1, 14075, 19399 +225, 10))
 OBJECTS.append(("Chest",1, 16383, 9400 +225, 30))
 OBJECTS.append(("Chest",1, 18419, 21400 +225, 20))
 OBJECTS.append(("Chest",1, 29100, 9000 +225, 20))
+OBJECTS.append(("Chest",1, 37700, 14200 +225, 20))
 
 OBJECTS.append(("Chest",2, 41200, 18199 +250, 50))
-OBJECTS.append(("Chest",2, 61500, 21800 +250, 50))
 OBJECTS.append(("Chest", 2, 40950, 8599 +225, 60))
-OBJECTS.append(("Chest", 2, 29700, 23000+225, 60))
 OBJECTS.append(("Chest", 2, 52400, 21800 +225, 80))
 OBJECTS.append(("Chest", 2, 57400, 7400 +225, 60))
 OBJECTS.append(("Chest", 2, 94600, 28600 +225, 80))
@@ -31,16 +28,21 @@ OBJECTS.append(("Chest", 2, 115650, 45400 +225, 100))
 OBJECTS.append(("Chest", 2, 117600, 35000 +225, 100))
 
 # OBJECTS.append(("Chest",3,6010,221200 +300, 90)) //salle troll
+OBJECTS.append(("Chest", 3, 29700, 23000+225, 60))
+OBJECTS.append(("Chest", 3, 44900, 28600+225, 70))
+OBJECTS.append(("Chest",3, 61500, 21800 +250, 90))
 OBJECTS.append(("Chest",3,82190,30200 +225, 100))
 OBJECTS.append(("Chest",3,118200,31800 +225, 100))
-OBJECTS.append(("Chest",3,105000,45400 +225, 120))
+OBJECTS.append(("Chest",3,105000,44200 +225, 120))
 OBJECTS.append(("Chest",3,116300,37800 +225, 120))
 OBJECTS.append(("Chest", 3, 62150, 35400 +225, 100))
+OBJECTS.append(("Chest", 3, 90400, 45400 +225, 100))
 
 OBJECTS.append(("Chest",4,48298,14599 +225, 170))
 OBJECTS.append(("Chest",4,117500,21800 +225, 170))
 #OBJECTS.append(("Chest",4,97331,52600 +225, 170)) #Put it just before
 OBJECTS.append(("Chest",4,94250,35400 +225, 170))
+OBJECTS.append(("Chest",4,116700,50200 +225, 170))
 
 OBJECTS.append(("SPELL",2,1300,18500,1)) #Put a pnj instead
 
